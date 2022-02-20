1311488f7f8be2c64b7e77d14ed4fb494a4f7c62
AndreyCorpse280990 <280990corpse@gmail.com>
2022-02-20 13:15:38 +0300

    done

--- date.py
+++ date.py
@@ -5,8 +5,10 @@ one_day = timedelta(days=1)
 yesterday_date = dt_now - one_day
 print(yesterday_date)
 print(dt_now)
-
 month_delta = timedelta(days=30)
 month_date = dt_now - month_delta
 print(month_date)
 
+date_srt = "01/01/25 12:10:03.234567"
+date_dt = datetime.strptime(date_srt, '%d/%m/%y %H:%M:%S.%f')
+print(date_dt)
